49fa73b3fc3617248b0c2bb799782165dbd876a9
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-07-05 16:25:31 +0700

    List used assets credits

---
+++ Assets/FPS/Scripts/UI/OpenLinkButton.cs
@@ -0,0 +1,25 @@
+﻿using Unity.FPS.Game;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Unity.FPS.UI
+{
+    public class OpenLinkButton : MonoBehaviour
+    {
+        public string linkUrl = "";
+
+        void Update()
+        {
+            if (EventSystem.current.currentSelectedGameObject == gameObject
+                && Input.GetButtonDown(GameConstants.k_ButtonNameSubmit))
+            {
+                LoadTargetUrl();
+            }
+        }
+
+        public void LoadTargetUrl()
+        {
+            Application.OpenURL(linkUrl);
+        }
+    }
+}
